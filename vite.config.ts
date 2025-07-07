b0163d83bdd656ce8ca09977f875edea17ab5593
rakanssh <rsshammri@gmail.com>
2025-07-07 12:44:34 +0300

    chore: renamed src to app

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(async () => ({
 
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "./src"),
+      "@": path.resolve(process.cwd(), "./app"),
     },
   },
 
