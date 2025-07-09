374ce201f037c089a3ef0d30ad5d0fdb9cdea715
rakanssh <112320286+rakanssh@users.noreply.github.com>
2025-07-09 23:57:18 +0300

    feat: extend settings store to include model management

--- app/store/useSettingsStore.ts
+++ app/store/useSettingsStore.ts
@@ -1,3 +1,4 @@
+import { LLMModel } from "@/services/llm/schema";
 import { create } from "zustand";
 
 const enum ApiType {
@@ -7,13 +8,17 @@ const enum ApiType {
 interface SettingsStoreType {
   apiKey: string;
   apiType: ApiType;
+  model: LLMModel | undefined;
   setApiKey: (apiKey: string) => void;
   setApiType: (apiType: ApiType) => void;
+  setModel: (model: LLMModel) => void;
 }
 
 export const useSettingsStore = create<SettingsStoreType>((set) => ({
   apiKey: "",
   apiType: ApiType.OPENROUTER,
+  model: undefined,
   setApiKey: (apiKey: string) => set({ apiKey }),
   setApiType: (apiType: ApiType) => set({ apiType }),
+  setModel: (model: LLMModel) => set({ model }),
 }));
