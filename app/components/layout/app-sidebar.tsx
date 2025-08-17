bca8f544f3bc1a6de583b8894e0a1a84fbf9eaf3
rakanssh <112320286+rakanssh@users.noreply.github.com>
2025-08-18 00:24:57 +0300

    feat: make controls slightly less ugly

--- app/components/layout/app-sidebar.tsx
+++ app/components/layout/app-sidebar.tsx
@@ -2,13 +2,21 @@ import * as React from "react";
 
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
-import { ArrowLeftIcon, RedoIcon, UndoIcon } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  ArrowRightToLine,
+  Loader2,
+  RedoIcon,
+  Trash2,
+  UndoIcon,
+} from "lucide-react";
 import { useNavigate } from "react-router";
 import { useGameStore } from "@/store/useGameStore";
 import { Separator } from "@/components/ui/separator";
 import { InventoryCard, StatsCard } from "@/components/game";
 import { SettingsButton } from "@/components/layout/settings-button";
 import { useState } from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { CardContent, SquareCard } from "../ui/card";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
@@ -34,39 +42,71 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <Separator />
         <InventoryCard />
         <StatsCard />
-        <div className="flex flex-col mt-auto ">
-          <Separator />
-          <SquareCard className="p-0 m-0">
-            <CardContent className="flex flex-col gap-0 p-0 m-0">
-              <div className="flex flex-row gap-2 justify-between my-1 ">
-                <Button variant="ghost" size="icon" onClick={undo}>
-                  <UndoIcon className="w-4 h-4" />
-                </Button>
-                <Button
-                  className="flex-1"
-                  variant={confirmingReset ? "destructive" : "ghost"}
-                  onClick={handleReset}
-                >
-                  {confirmingReset ? "Are you sure?" : "Reset"}
-                </Button>
-                <Button variant="ghost" size="icon" onClick={redo}>
-                  <RedoIcon className="w-4 h-4" />
-                </Button>
-              </div>
-              {/* Separator half size of the container */}
-              <Separator />
-              <div className="flex flex-row gap-2 justify-between my-1">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => navigate("/")}
-                >
-                  <ArrowLeftIcon className="w-4 h-4" />
-                </Button>
-                <SettingsButton />
-              </div>
-            </CardContent>
-          </SquareCard>
+        <div className="flex flex-col mt-auto">
+          <Separator className="mb-1.5" />
+          <div className="py-0 px-0 ">
+            <div className="flex flex-row items-center justify-between gap-1 ">
+              <SquareCard className="p-0 m-0 flex-1">
+                <CardContent className="flex flex-row gap-0 p-0 m-0 ">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        onClick={() => navigate("/")}
+                        className="flex-1"
+                      >
+                        <ArrowLeftIcon className="w-4 h-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Home</TooltipContent>
+                  </Tooltip>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <span>
+                        <SettingsButton className="flex-1" />
+                      </span>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Settings</TooltipContent>
+                  </Tooltip>
+                </CardContent>
+              </SquareCard>
+              <SquareCard className="p-0 m-0 flex-1">
+                <CardContent className="flex flex-row gap-0 p-0 m-0">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon" onClick={undo}>
+                        <UndoIcon className="w-4 h-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Undo (Ctrl+Z)</TooltipContent>
+                  </Tooltip>
+
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        className="flex-1"
+                        variant={confirmingReset ? "destructive" : "ghost"}
+                        onClick={handleReset}
+                      >
+                        <Trash2 className="w-4 h-4 " />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Reset</TooltipContent>
+                  </Tooltip>
+
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon" onClick={redo}>
+                        <RedoIcon className="w-4 h-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Redo (Ctrl+Y)</TooltipContent>
+                  </Tooltip>
+                </CardContent>
+              </SquareCard>
+            </div>
+          </div>
         </div>
       </SidebarContent>
     </Sidebar>
--- app/components/layout/settings-button.tsx
+++ app/components/layout/settings-button.tsx
@@ -2,13 +2,16 @@ import { Button } from "../ui/button";
 import { SettingsIcon } from "lucide-react";
 import { useNavigate } from "react-router";
 
-export function SettingsButton() {
+export function SettingsButton({
+  ...props
+}: React.ComponentProps<typeof Button>) {
   const navigate = useNavigate();
   return (
     <Button
       variant="ghost"
       size="icon"
       onClick={() => navigate("/demo/settings/api", { replace: false })}
+      {...props}
     >
       <SettingsIcon className="w-4 h-4" />
     </Button>
