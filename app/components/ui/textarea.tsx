0e63f447110c04b5bffc1c5c8bdf70994b5ab28a
rakanssh <112320286+rakanssh@users.noreply.github.com>
2025-08-19 20:51:23 +0300

    feat: uniform scrollbar style

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -26,6 +26,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 
     return (
       <textarea
+        data-slot="textarea"
         className={cn(
           "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex w-full min-w-0 rounded-md border bg-transparent px-3 py-2 min-h-9 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-auto file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
