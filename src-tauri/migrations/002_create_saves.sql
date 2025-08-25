9ef5f513a72ad4241d5cf273291fedd10d54bd6b
rakanssh <112320286+rakanssh@users.noreply.github.com>
2025-08-25 09:53:01 +0300

    fix: set FK null when scenario deleted

--- src-tauri/migrations/002_create_saves.sql
+++ src-tauri/migrations/002_create_saves.sql
@@ -1,12 +1,12 @@
 CREATE TABLE
     IF NOT EXISTS saves (
         id TEXT PRIMARY KEY,
-        scenario_id TEXT NOT NULL,
+        scenario_id TEXT,
         save_name TEXT NOT NULL,
         stats TEXT NOT NULL,
         inventory TEXT NOT NULL,
         log TEXT NOT NULL,
         version INTEGER NOT NULL DEFAULT 1,
         created_at INTEGER NOT NULL,
-        FOREIGN KEY (scenario_id) REFERENCES scenarios (id)
+        FOREIGN KEY (scenario_id) REFERENCES scenarios (id) ON DELETE SET NULL
     );
